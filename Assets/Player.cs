d8607ddda8e3b3d7badc8d3b069159883925622e
Ben Tristem <ben@bentristem.com>
2015-06-19 09:05:44 +0100

    ZR13 Setup Player Spawn Points.mp4 (v.2)

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Transform playerSpawnPoints; // The parent of the spawn points
+	public bool reSpawn = false;
+	
+	private Transform[] spawnPoints;
+	private bool lastToggle = false;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastToggle != reSpawn) {
+			Respawn ();
+			reSpawn = false;
+		} else {
+			lastToggle = reSpawn;
+		}
+	}
+
+	private void Respawn() {
+		int i = Random.Range (1, spawnPoints.Length);
+		transform.position = spawnPoints [i].transform.position;
+	}
+}
