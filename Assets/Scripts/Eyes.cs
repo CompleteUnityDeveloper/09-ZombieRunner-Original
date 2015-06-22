5c93193d43028ab6ef8865e2dc9e64d5dfb89dc5
Ben Tristem <ben@bentristem.com>
2015-06-22 19:14:48 +0100

    ZR14 Using Input Mapping.mp4

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera eyes;
+	private float defaultFOV;
+
+	// Use this for initialization
+	void Start () {
+		eyes = GetComponent<Camera> ();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Zoom")) {
+			eyes.fieldOfView = defaultFOV / 1.5f;
+		} else {
+			eyes.fieldOfView = defaultFOV;
+		}
+	}
+}
---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip callSound;
+
+	private bool called = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("CallHeli") && !called) {
+			called = true;
+			print ("Helicopter called");
+		}
+	}
+}
