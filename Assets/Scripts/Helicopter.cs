186b2d9576a942caad16eef05014acfc7e5639d9
Ben Tristem <ben@bentristem.com>
2015-06-22 19:40:52 +0100

    ZR15 Adding 3D Helicopter Sound.mp4

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -6,17 +6,20 @@ public class Helicopter : MonoBehaviour {
 	public AudioClip callSound;
 
 	private bool called = false;
+	private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
-	
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("CallHeli") && !called) {
 			called = true;
-			print ("Helicopter called");
+			Debug.Log ("Helicopter called");
+			audioSource.clip = callSound;
+			audioSource.Play();
 		}
 	}
 }
