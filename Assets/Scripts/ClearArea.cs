7871bd7ce74bcb5114e5c4cc8aa7fdbf6492c33d
Ben Tristem <ben@bentristem.com>
2015-07-06 19:02:01 +0100

    ZR19 Searching For Clear Area.mp4

---
+++ Assets/Scripts/ClearArea.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearArea : MonoBehaviour {
+
+	public float timeSinceLastTrigger = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastTrigger += Time.deltaTime;
+
+		if (timeSinceLastTrigger > 1f) {
+			Debug.Log ("Found clear area");
+
+		}
+	}
+
+	void OnTriggerStay () {
+		timeSinceLastTrigger = 0f;
+	}
+}
