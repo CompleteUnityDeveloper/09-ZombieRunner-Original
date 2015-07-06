d8361cc1c6ad1e2276f076355b5a1cd4eff1d2ca
Ben Tristem <ben@bentristem.com>
2015-07-06 15:59:58 +0100

    ZR18 Simple Day-Night Cycle.mp4

---
+++ Assets/Scripts/Daycycle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Daycycle : MonoBehaviour {
+
+	[Tooltip ("Number of minutes per second that pass, try 60")]
+	public float minutesPerSecond;
+
+	private Quaternion startRotation;
+
+	// Use this for initialization
+	void Start () {
+		startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angleThisFrame = Time.deltaTime / 360 * minutesPerSecond;
+		transform.RotateAround (transform.position, Vector3.forward, angleThisFrame);
+	}
+}
