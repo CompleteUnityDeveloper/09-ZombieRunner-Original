6dd95b993078dba3df3e7a9167bd66b382e1573c
Ben Tristem <ben@bentristem.com>
2015-07-20 13:22:21 +0100

    ZR22 Using BroadcastMessage().mp4 (DOES NOT COMPILE)

--- Assets/Scripts/Daycycle.cs
+++ Assets/Scripts/Daycycle.cs
@@ -5,13 +5,6 @@ public class Daycycle : MonoBehaviour {
 
 	[Tooltip ("Number of minutes per second that pass, try 60")]
 	public float minutesPerSecond;
-
-	private Quaternion startRotation;
-
-	// Use this for initialization
-	void Start () {
-		startRotation = transform.rotation;
-	}
 	
 	// Update is called once per frame
 	void Update () {
